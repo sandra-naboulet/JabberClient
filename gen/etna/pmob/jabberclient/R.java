1e881ba6e88e7707b30e49b85986400ab505a51e
Sandra Naboulet <naboul_s@etna-alternance.net>
2014-08-02 12:38:49 +0200

    Login page

--- gen/etna/pmob/jabberclient/R.java
+++ gen/etna/pmob/jabberclient/R.java
@@ -20,22 +20,26 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
     }
     public static final class color {
         public static final int black_overlay=0x7f040000;
+        public static final int primary=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int background=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int dummy_button=0x7f070002;
-        public static final int fullscreen_content=0x7f070000;
-        public static final int fullscreen_content_controls=0x7f070001;
+        public static final int login_button=0x7f070002;
+        public static final int login_password=0x7f070001;
+        public static final int login_username=0x7f070000;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_login=0x7f030000;
     }
     public static final class string {
         public static final int app_name=0x7f050000;
-        public static final int dummy_button=0x7f050001;
-        public static final int dummy_content=0x7f050002;
+        public static final int login=0x7f050003;
+        public static final int password=0x7f050002;
+        public static final int signup_link=0x7f050004;
+        public static final int username=0x7f050001;
     }
     public static final class style {
         /** 
